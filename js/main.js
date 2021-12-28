590ae4b37af3062d88124c284e42ea496c694bbf
quangdo <quangdm@cdmteck.com>
2021-12-28 18:53:05 +0700

    #init about us screen

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
 const $ = require('jquery');
+window.jQuery = require('jquery');
 const header = require('./modules/header');
+slick = require('slick-carousel');
 $(function () {
     header();
-    console.log('123');
+    $('.koni-hp-ourteam-slick-wrapper').slick({
+        vertical: true,
+        verticalSwiping: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1646,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1490,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    })
+
+    $('.koni-hp-ouradvisor-slicker-wrapper').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    })
 });
